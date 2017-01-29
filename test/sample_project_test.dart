d467bae6a69ff55b9458400cf5d7ab34a87cb3d6
Daniel Varga <dvarga@skawa.hu>
2017-01-29 20:26:38 +0100

    Added some tests

---
+++ test/sample_project_test.dart
@@ -0,0 +1,45 @@
+@TestOn('vm')
+import 'dart:io';
+
+import 'package:heron/src/data.dart';
+import 'package:scheduled_test/scheduled_test.dart';
+import 'package:scheduled_test/descriptor.dart' as d;
+import 'package:heron/heron.dart' show PathConfig;
+import 'package:markdown/markdown.dart' as md;
+
+import 'utils.dart';
+
+void main() {
+  // TODO: should really be setUpAll, once dart-lang/scheduled_test#28 is fixed, replace
+  PathConfig pathConfig;
+  setUp(() {
+    setupSandbox(cleanUpAfterTests: !EnvVars.NOCLEANUP);
+    sampleProject.create();
+    runHeron(inDirectory: sandboxPath)
+      ..shouldExit(0);
+    pathConfig = new PathConfig.defaults();
+  });
+
+  test('static files copied', () {
+    validateSandboxed(robotsTxt, pathConfig.siteOutput);
+    validateSandboxed(runJs, pathConfig.siteOutput);
+    validateSandboxed(styleCss, pathConfig.siteOutput);
+  });
+
+  test('contains html-head', () {
+    var content = new MarkdownContent.parseSource(indexMd.textContents);
+    expect(content.header, isNotNull);
+    print(content.header.asMap);
+//    var lines = indexHtml.textContents;
+//    var a = new md.Document().parseLines(lines);
+//    List<md.Node> nodes = new md.Document()
+//        .parseInline(indexMd.textContents);
+//    print(a);
+//    md.markdownToHtml(indexMd.textContents)
+//    indexMd.textContents;
+//    var htmlHeadTemplate = d.file('index.html');
+//    d.matcherFile('index.html', contains(''));
+//    validateSandboxed(indexHtml, pathConfig.siteOutput);
+//    indexHtml.textContents.contains();
+  });
+}
